3d9b34d7bbb93edc12751a8fbf1004da310acac8
Goodzimat <118252528+Goodzimat@users.noreply.github.com>
2022-11-17 00:25:06 +0100

    Rangements
    Separation en sections et modification de quelques nom de fonctions ambigus dans main.py
    Ajout des premieres modif de nabila pour le saut

--- Tombe&Saute.py
+++ Tombe&Saute.py
@@ -1,15 +1,81 @@
 import pygame
 
-BLEU_CIEL = (135, 206, 255)
- 
+BLEU_CIEL = (135, 206, 250)
+
 FENETRE_LARGEUR = 800
 FENETRE_HAUTEUR = 600
 
+BALE_LARGEUR = 60
+BALE_HAUTEUR = 51
+
+
 SOL_HAUTEUR = 500
-LIMITE_MORT = 575
 
 X = 0
 Y = 1
+
+pygame.init()
+
+fenetre_taille = (FENETRE_LARGEUR, FENETRE_HAUTEUR)
+fenetre = pygame.display.set_mode(fenetre_taille)
+pygame.display.set_caption('BALE')
+
+##### Définition ENTITE #####
+
+def nouvelleEntite():
+    return {
+        'visible':False,
+        'position': [0, 0],
+        'vitesse':[0,0],
+        'acceleration':[0,0],
+        'imageAffichee':None,
+        'poses': []
+    }
+
+def visible(entite):
+    entite['visible'] = True
+
+
+def invisible(entite):
+    entite['visible'] = False
+
+
+def estVisible(entite):
+    return entite['visible']
+
+
+def place(entite, x, y):
+    entite['position'][0] = x
+    entite['position'][1] = y
+
+def set_vitesse(entite, x, y):
+    entite['vitesse'][0] = x
+    entite['vitesse'][1] = y
+
+def set_acceleration(entite, x, y):
+    entite['acceleration'][0] = x
+    entite['acceleration'][1] = y
+
+def position(entite):
+    return entite['position']
+
+
+def ajoutePose(entite, image):
+    entite['poses'].append(image)
+
+
+def prendsPose(entite, pose):
+    entite['imageAffichee'] = entite['poses'][pose]
+    visible(entite)
+
+def dessine(entite, ecran):
+    ecran.blit(entite['imageAffichee'], entite['position'])
+
+
+##### Fin ENTITE #####
+
+
+    
 
 
 def traite_entrees() :
@@ -19,66 +85,72 @@ def traite_entrees() :
          fini = True
       if evenement.type == pygame.KEYDOWN:
          if evenement.key == pygame.K_UP:
-            saute(Obj)
+            saute(bale)
 
-def affiche_objet(fenetre, objet):
-   pygame.draw.circle(fenetre, (255,0,0), objet['pos'], objet['rad'])
 
-def newPos(objet):
+
+def newPosBalle(objet):
    global temps_precedent, fini
    delai = temps_maintenant - temps_precedent
    
-   objet['vit'][X] = objet['vit'][X] + objet['acc'][X] * delai
-   objet['vit'][Y] = objet['vit'][Y] + objet['acc'][Y] * delai
 
-   objet['pos'][X] = int(objet['pos'][X] + objet['vit'][X] * delai)
-   objet['pos'][Y] = int(objet['pos'][Y] + objet['vit'][Y] * delai)
+   set_vitesse(objet, objet['vitesse'][X] + objet['acceleration'][X] * delai,objet['vitesse'][Y] + objet['acceleration'][Y] * delai)
+   
+   place(objet,int(objet['position'][X] + objet['vitesse'][X] * delai),  int(objet['position'][Y] + objet['vitesse'][Y] * delai))
+   
  
    temps_precedent = temps_maintenant
    
    
-   if objet['pos'][Y] + objet['rad'] > LIMITE_MORT :
-      fini = True
-   if objet['pos'][Y] + objet['rad'] > SOL_HAUTEUR :
-      objet['pos'][Y] = SOL_HAUTEUR - objet['rad']
-      objet['vit'][Y] = 0
+#    if objet['pos'][Y] + objet['rad'] > LIMITE_MORT :
+#       fini = True
+   if objet['position'][Y] + BALE_HAUTEUR > SOL_HAUTEUR :
+      objet['position'][Y]= SOL_HAUTEUR - BALE_HAUTEUR
+      objet['vitesse'][Y]=0
+      
 
-   if objet['pos'][X] > FENETRE_LARGEUR :
-      objet['pos'][X] = 0
-   elif objet['pos'][X] < 0 :
-      objet['pos'][X] = FENETRE_LARGEUR
+   if objet['position'][X] > FENETRE_LARGEUR :
+      objet['position'][X] = 0
+   elif objet['position'][X] < 0 :
+      objet['position'][X] = FENETRE_LARGEUR
 
-Obj = {'pos':[400,300], 'vit':[0,0], 'acc':[0,0.001], 'rad':25}
+
+
 
 def saute(objet):
-   if objet['pos'][Y] == SOL_HAUTEUR - objet['rad']:
-      Obj['vit'][Y] = -0.5
+   if objet['position'][Y] == SOL_HAUTEUR - BALE_HAUTEUR:
+      set_vitesse(objet,0,-0.5)
 
 
-pygame.init()
 
-fenetre_taille = (FENETRE_LARGEUR, FENETRE_HAUTEUR)
-fenetre = pygame.display.set_mode(fenetre_taille)
-pygame.display.set_caption('Jeu')
-
+bale = nouvelleEntite()
+set_acceleration(bale,0,0.001)
+place(bale,400,300)
+chemin = './images/cricket-ball.png' 
+image = pygame.image.load(chemin).convert_alpha(fenetre)
+image = pygame.transform.scale(image, (BALE_LARGEUR, BALE_HAUTEUR))
+bale['imageAffichee']=image
 fini = False
 temps = pygame.time.Clock()
+
 temps_precedent = 0
-
 while not fini:
-   traite_entrees()
- 
-   fenetre.fill(BLEU_CIEL)
-   pygame.draw.line(fenetre, (0,0,0), (0, SOL_HAUTEUR), (FENETRE_LARGEUR, SOL_HAUTEUR))
 
-   temps_maintenant = pygame.time.get_ticks()
 
-   affiche_objet(fenetre, Obj)
-   newPos(Obj)
+    traite_entrees()
+    
 
-   pygame.display.flip()
+    fenetre.fill(BLEU_CIEL)
 
-   temps.tick(50)
+    temps_maintenant = pygame.time.get_ticks()
+
+    dessine(bale, fenetre)
+    newPos(bale)
+    
+
+    pygame.display.flip()
+
+    temps.tick(50)
 
 pygame.display.quit()
 pygame.quit()
--- main.py
+++ main.py
@@ -7,16 +7,163 @@ NOIR = (0, 0, 0)
 FENETRE_LARGEUR = 800
 FENETRE_HAUTEUR = 600
 
-# Fin Constantes
+FENETRE_MARGE_EXTERNE = 200
+FENETRE_MARGE_INTERNE = 50
+
+SOL_HAUTEUR = 370
+SOL_LARGEUR = 120
 
 # Paramètres
 
 dimensions_fenetre = (FENETRE_LARGEUR, FENETRE_HAUTEUR)
 images_par_seconde = 25
 
-# Fin Paramètres
-
 # Fonctions
+
+#### Definition Entitee ####
+
+def nouvelleEntite(nom, image):
+    return {
+        "nom": nom,
+        "visible": False,
+        "taille": [0,0],
+        "position": [0, 0],
+        "vitesse": [0, 0],
+        "acceleration": [0, 0],
+        "dernierTemps": 0,
+        "image": image,
+        "angleRotation": 0
+    }
+
+
+def visible(entite):
+    entite['visible'] = True
+
+def invisible(entite):
+    entite['visible'] = False
+
+def estVisible(entite):
+    return entite['visible']
+
+
+def place(entite, x, y):
+    entite['position'][0] = x
+    entite['position'][1] = y
+
+def set_vitesse(entite, x, y):
+    entite['vitesse'][0] = x
+    entite['vitesse'][1] = y
+
+def set_acceleration(entite, x, y):
+    entite['acceleration'][0] = x
+    entite['acceleration'][1] = y
+
+def position(entite):
+    return entite['position']
+
+
+def set_taille(entite, largeur, hauteur):
+    entite["taille"][0] = largeur
+    entite["taille"][1] = hauteur
+
+def taille(entite):
+    return entite["taille"]
+
+def miseAJourEntite(scene):
+    nbr = nombreElementScene(scene)
+    
+    if nbr > 0:
+        pass
+
+
+def afficheEntite(scene):
+    global fenetre
+    entites = scene["entites"]
+    
+    for entite in entites:
+        fenetre.blit(entite["image"], repere_vers_pygame(entite["position"]))
+
+
+#### Fin Entitee ####
+
+#### Definition Scene ####
+     
+def nouvelleScene():
+    return {
+        "entites": []
+    }
+    
+
+def ajouterEntite(scene, entite):
+    scene["entites"].append(entite)
+    
+
+def nombreElementScene(scene):
+    return len(scene["entites"])
+
+def derniereEntite(scene):
+    nbr = nombreElementScene(scene)
+    
+    if nbr > 0:
+        return scene["entites"][-1]
+    return None
+
+def premiereEntite(scene):
+    nbr = nombreElementScene(scene)
+
+    if nbr(scene) > 0:
+        return scene["entites"][0]
+    return None
+
+#### Fin Scene ####
+
+#### Sol ####
+
+def generer_sol(position):
+    entite = nouvelleEntite("sol", IMAGE_SOL)
+    place(entite, position, 100)
+    set_taille(entite, SOL_LARGEUR, SOL_HAUTEUR)
+    entite["dernierTemps"] = pygame.time.get_ticks()
+    
+    return entite
+
+
+def get_sol(scene):
+    entites = scene["entites"]
+    sols = []
+
+    for entite in entites:
+        if entite["nom"] == "sol":
+            sols.append(entite)
+
+    return sols
+
+
+def updateSol(scene):
+    sols = get_sol(scene)
+    
+    if len(sols) > 0:
+        while position(sols[0]) < -FENETRE_MARGE_EXTERNE:
+            scene["entites"].remove(sols[0])
+            sols.remove(0)
+
+    derniere_position = (0,0)
+    derniere_taille = (0,0)
+ 
+    if len(sols) > 0:
+        dernier = sols[-1]
+        derniere_position = position(dernier) 
+        derniere_taille = taille(dernier)
+
+    while derniere_position[0] + derniere_taille[0] < FENETRE_LARGEUR + FENETRE_MARGE_EXTERNE:
+        entite = generer_sol(derniere_position[0] + derniere_taille[0])
+        visible(entite)
+        ajouterEntite(scene, entite)
+
+        derniere_position = position(entite)
+        derniere_taille = taille(entite)
+
+#### Fin Sol ####
 
 def traite_entrees():
     global fini
@@ -25,14 +172,42 @@ def traite_entrees():
         if evenement.type == pygame.QUIT:
             fini = True
 
+
+def afficherBack(fenetre, background):
+    fenetre.blit(background, (0,0))
+    
+
+def afficherEcranDeJeu():
+    global scene
+    
+    #miseAJourEntite(scene)
+    updateSol(scene)
+    afficherBack(fenetre, IMAGE_SCENE)
+    afficheEntite(scene)
+    
+
+def repere_vers_pygame(position):
+    return (position[0], FENETRE_HAUTEUR - position[1])
+
+
+def load_image(fenetre, chemin, dimensions):
+    IMAGE = pygame.image.load(chemin).convert_alpha(fenetre)
+    return pygame.transform.scale(IMAGE, dimensions)
+
+#### Fin Fonctions ####
+
 # Initialisation
 pygame.init()
 
 fenetre = pygame.display.set_mode(dimensions_fenetre)
 pygame.display.set_caption( "Bounce Tales" )
 
+IMAGE_SOL = load_image(fenetre, "images/ground.png", (SOL_LARGEUR, SOL_HAUTEUR))
+IMAGE_SCENE = load_image(fenetre, "images/trees-7191822_1280.png", (FENETRE_LARGEUR, FENETRE_HAUTEUR))
+
 fini = False
 enJeu = False
+scene = nouvelleScene()
 horloge = pygame.time.Clock()
 temps_depart = pygame.time.get_ticks()
 
@@ -41,8 +216,9 @@ while not fini:
     
     fenetre.fill(NOIR)
     
-    pygame.display.flip()
+    afficherEcranDeJeu()
     
+    pygame.display.flip()
     horloge.tick(images_par_seconde)
 
 pygame.display.quit()
